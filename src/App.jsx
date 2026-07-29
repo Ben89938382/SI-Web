bcf202a474e7f4b29b8235e0583a0bca0dfcd2fa
Ben89938382 <benjaminb0813@gmail.com>
2026-07-29 17:15:16 -0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+export default function App() {
+  return (
+    <div style={{
+      minHeight: "100vh",
+      background: "black",
+      color: "white",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      fontSize: "32px"
+    }}>
+      Sniper Industries Working
+    </div>
+  )
+}
